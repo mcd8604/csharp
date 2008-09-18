29de51efa254445082bd9fa029a20aded7f23c8f
mcd8604 <mcd8604@users.noreply.github.com>
2008-09-18 00:21:37 +0000

    Xcel - added XcelProduct

---
+++ Assignment1/Xcel/XcelProduct.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TerryAndMike.Xcel
+{
+    public class XcelProduct : XcelCommand
+    {
+        protected int? _product = null;
+
+        protected override string CommandName
+        {
+            get { return "product"; }
+        }
+
+        public XcelProduct() { }
+
+        #region XcelCommand Members
+
+        public override void Execute()
+        {
+            if (!_product.HasValue)
+            {
+                foreach (int i in _args)
+                    _product *= i;
+            }
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return _product.ToString();
+        }
+    }
+}
