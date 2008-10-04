bbdeb6710441dee58c6af84ad4e77f48d40e3ab5
mcd8604 <mcd8604@users.noreply.github.com>
2008-10-04 05:31:48 +0000

    Silverlight - part a: MVC interfaces

---
+++ Silveright/MVC/IController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TerryAndMike.MVC
+{
+    /// <summary>
+    /// Defines a change in state.
+    /// </summary>
+    /// <param name="row">The row of the tile.</param>
+    /// <param name="col">The column of the tile.</param>
+    /// <param name="tile">The tile to set.</param>
+    public delegate void State(int row, int col, int tile);
+
+    /// <summary>
+    /// Associates an IModel with multiple IViews
+    /// </summary>
+    public interface IController
+    {
+
+        /// <summary>
+        /// The Model owned by the controller
+        /// </summary>
+        IModel Model
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Registers view as an observer of the controller's model.
+        /// </summary>
+        /// <param name="view">The IView to add.</param>
+        void AddView(IView view);
+
+        /// <summary>
+        /// Unregisters view as an observer of the controller's model.
+        /// </summary>
+        /// <param name="view">The IView to remove.</param>
+        void RemoveView(IView view);
+    }
+}
