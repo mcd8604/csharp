d592c8cb47a079945820ddca62d916bba121ffd6
mcd8604 <mcd8604@users.noreply.github.com>
2008-10-06 02:00:02 +0000

    converted MVC to silverlight class library, Added empty PuzzleGame as project to implement Controller and View

---
+++ Silveright/PuzzleGame/Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace PuzzleGame
+{
+    public partial class Page : UserControl
+    {
+        public Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Silveright/StateMVC/IView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TerryAndMike.SilverlightGame.StateMVC
+{
+    /// <summary>
+    /// Represents the state of an IModel to the user.
+    /// </summary>
+    public interface IView
+    {
+        /// <summary>
+        /// Updates the IView to represent the state of an IModel.
+        /// </summary>
+        /// <param name="row">The row of the tile.</param>
+        /// <param name="col">The column of the tile.</param>
+        /// <param name="tile">The tile that was set.</param>
+        void StateUpdated(int row, int col, int tile);
+    }
+}
