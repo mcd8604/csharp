bd94dcd94398ab796f44192ea6e6a197b5d36595
atv7683 <atv7683@099f2f60-5456-0410-b76e-8cc51cc0753e>
2008-10-14 19:20:12 +0000

    Added skeleton for BlackoutModel

---
+++ Silverlight/StateMVC/BlackoutModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TerryAndMike.SilverlightGame.StateMVC
+{
+    public class BlackoutModel : IModel
+    {
+
+        #region IModel Members
+
+        public void NotifyStateChange(int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset(int rows, int cols)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddView(IView view)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveView(IView view)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
