941c185d3eee98471bd210993c27e57d54aa041b
atv7683 <atv7683@099f2f60-5456-0410-b76e-8cc51cc0753e>
2008-10-15 11:01:59 +0000

    changed mind, decided one ModelTestDriver for all model types is sufficient

---
+++ Silverlight/ModelTestDriver/ModelTestDriver.cs
@@ -0,0 +1,97 @@
+﻿using Console = System.Console;
+using TerryAndMike.SilverlightGame.StateMVC;
+
+namespace TerryAndMike.SilverlightGame.TestConsole
+{
+    /// <summary>
+    /// A simple console test client, or driver, to test the PuzzleModel
+    /// </summary>
+    class ModelTestDriver : IView, IController
+    {
+        private IModel model;
+
+        public ModelTestDriver( IModel model )
+        {
+            this.model = model;
+            this.model.AddView(this);
+        }
+
+        #region IView Members
+
+        public void StateUpdated(int row, int col, int tile)
+        {
+            //tile of zero indicates blank space
+//            if ( tile != 0 )
+                Console.WriteLine(row+","+col+" "+tile);
+        }
+
+        #endregion
+
+        #region IController Members
+
+        public void NotifyStateChange(int row, int col)
+        {
+            model.NotifyStateChange(row, col);
+        }
+
+        public void Reset(int row, int col)
+        {
+            model.Reset(row, col);
+        }
+
+        #endregion
+
+
+
+        static void Main(string[] args)
+        {
+            IModel startModel = null;
+            
+            Console.Write( "Choose model type to test: \n\t(1) Puzzle \n\t(2) Blackout\n\n?: " );
+            string modelType = Console.ReadLine();
+            switch ( modelType ) {
+                case "1":
+                case "puzzle":
+                case "Puzzle":
+                    startModel = new PuzzleModel();
+                    break;
+                case "2":
+                case "blackout":
+                case "Blackout":
+                    startModel = new BlackoutModel();
+                    break;
+            }
+
+            ModelTestDriver myDriver;
+
+            try {
+                myDriver = new ModelTestDriver( startModel );
+                string[] rowcol;
+                char[] spaceCharArray = new char[] { ' ' };
+
+                /**** Read in puzzle size and Reset() to initialize puzzle ****/
+                do {
+                    Console.Write( "Enter board size as \"#rows #cols\": " );
+                    rowcol = Console.ReadLine().Split( spaceCharArray );
+                } while ( rowcol.Length != 2 );
+
+                myDriver.Reset( int.Parse( rowcol[ 0 ] ), int.Parse( rowcol[ 1 ] ) );
+
+
+                /**** Loop until "quit" token, simulating clicks calling NotifyStateChange ****/
+                string input = string.Empty;
+                do {
+                    Console.Write( "\nEnter \"row col\" of tile to send state change or \"quit\" to quit: " );
+                    input = Console.ReadLine();
+                    rowcol = input.Split( spaceCharArray );
+                    if ( rowcol.Length == 2 ) {
+                        myDriver.NotifyStateChange( int.Parse( rowcol[ 0 ] ), int.Parse( rowcol[ 1 ] ) );
+                    }
+                } while ( !input.Equals( "quit", System.StringComparison.CurrentCultureIgnoreCase ) );
+            }
+            catch ( System.NullReferenceException ) {
+                Console.Error.WriteLine( "Invalid choice." );
+            }
+        }
+    }
+}
