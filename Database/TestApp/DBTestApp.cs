831c78838e842e58bffd487853b488ca1c8a835e
atv7683 <atv7683@099f2f60-5456-0410-b76e-8cc51cc0753e>
2008-10-22 22:07:23 +0000

    started test app, renamed class

---
+++ Database/TestApp/DBTestApp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using TextWriter = System.IO.TextWriter;
+using TextReader = System.IO.TextReader;
+using TerryAndMike.Database;
+using Axel.Database;
+
+
+namespace TerryAndMike.Database.TestApp {
+    class DBTestApp {
+
+        IModel model = new LocalDB();
+
+        public void CommandParseLoop( TextWriter outStream, TextReader inStream ) {
+            string command = string.Empty;
+            string[] commandTokens;
+
+            OutputInstructions( outStream );
+
+            while ( !command.Equals( "quit", StringComparison.CurrentCultureIgnoreCase ) ) {
+
+                outStream.Write( "Enter a command: " );
+                command = inStream.ReadLine();
+                commandTokens = command.Split( ' ' );
+
+                switch ( commandTokens[ 0 ] ) {
+                    case "add":
+                    case "Add":
+                        outStream.WriteLine( "adding" );
+                        break;
+                    case "extract":
+                    case "Extract":
+                        outStream.WriteLine( "extracting" );
+                        break;
+                    case "delete":
+                    case "Delete":
+                        outStream.WriteLine( "deleting" );
+                        break;
+                }
+            }
+        }
+
+        private void OutputInstructions( TextWriter outStream ) {
+            outStream.WriteLine( "Available commands:\n\tadd [word]...\n\textract value-position key-position pattern\n\tdelete key-position pattern" );
+        }
+
+        static void Main( string[] args ) {
+            
+
+            
+
+        }
+    }
+}
