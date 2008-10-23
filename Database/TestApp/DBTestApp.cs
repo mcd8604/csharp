0d2027b5ef4df0aa849d07ccd9296a223ac7c720
atv7683 <atv7683@099f2f60-5456-0410-b76e-8cc51cc0753e>
2008-10-23 02:42:15 +0000

    TestApp done

--- Database/TestApp/DBTestApp.cs
+++ Database/TestApp/DBTestApp.cs
@@ -8,6 +8,7 @@ using Axel.Database;
 
 namespace TerryAndMike.Database.TestApp {
     class DBTestApp {
+        private const int TUPLE_LENGTH = 3;
 
         IModel model = new LocalDB();
 
@@ -19,36 +20,100 @@ namespace TerryAndMike.Database.TestApp {
 
             while ( !command.Equals( "quit", StringComparison.CurrentCultureIgnoreCase ) ) {
 
-                outStream.Write( "Enter a command: " );
-                command = inStream.ReadLine();
+                outStream.Write( "\nEnter a command: " );
+                command = inStream.ReadLine().Trim();
                 commandTokens = command.Split( ' ' );
 
                 switch ( commandTokens[ 0 ] ) {
                     case "add":
                     case "Add":
-                        outStream.WriteLine( "adding" );
+                        OutputAddOperation( outStream, commandTokens );
                         break;
                     case "extract":
                     case "Extract":
-                        outStream.WriteLine( "extracting" );
+                        OutputExtractOperation( outStream, commandTokens );
                         break;
                     case "delete":
                     case "Delete":
-                        outStream.WriteLine( "deleting" );
+                        OutputDeleteOperation( outStream, commandTokens );
                         break;
                 }
             }
         }
 
+        private string[] GetPatternTuple( int keyFieldId, string key, int tupleLength ) {
+            string[] patternInputArray = new string[ tupleLength ];
+            for ( int i = 0; i < tupleLength; ++i )
+                patternInputArray[ i ] = ( i == keyFieldId ) ? key : null;
+            return patternInputArray;
+        }
+
+        /// <summary>
+        /// Search all tuples for a key in a specified key field, extracting corresponding value field specified
+        /// </summary>
+        /// <param name="valueFieldId">Zero-based field id for the requested value field</param>
+        /// <param name="keyFieldId">Zero-based field id for the search key</param>
+        /// <param name="key">Key to search for in field 'keyFieldId'</param>
+        /// <returns>Matrix (Nx2), representing N (matching length) 2-tuples, columns are key and value fields</returns>
+        private string[ , ] ExtractFieldByPattern( int valueFieldId, int keyFieldId, string key ) {
+            string[] patternInputArray = GetPatternTuple( keyFieldId, key, TUPLE_LENGTH );
+            string[][] dbResults = model.Search( patternInputArray );
+            string[ , ] resultMatrix = new string[ dbResults.Length, 2 ];
+
+            for ( int i = 0; i < dbResults.Length; ++i ) {
+                resultMatrix[ i, 0 ] = dbResults[ i ][ keyFieldId ];
+                resultMatrix[ i, 1 ] = dbResults[ i ][ valueFieldId ];
+            }
+            return resultMatrix;
+        }
+
+        private void OutputExtractOperation( TextWriter outStream, string[] commandTokens ) {
+            if ( commandTokens.Length != 4 )
+                return;
+
+            string[ , ] results = ExtractFieldByPattern( Int32.Parse( commandTokens[ 1 ] ), Int32.Parse( commandTokens[ 2 ] ), commandTokens[ 3 ] );
+
+            //output results
+            outStream.WriteLine( "(key, value)\n------------" );
+            for ( int i = 0; i < results.GetLength( 0 ); ++i ) {
+                outStream.WriteLine( '(' + results[ i, 0 ] + ", " + results[ i, 1 ] + ")" );
+            }
+        }
+
+        private void OutputDeleteOperation( TextWriter outStream, string[] commandTokens ) {
+            string[] patternInputArray = GetPatternTuple( Int32.Parse( commandTokens[ 1 ] ), commandTokens[ 2 ], TUPLE_LENGTH );
+            if ( model.Remove( patternInputArray ) )
+                Console.WriteLine( "All tuples matching input pattern have been removed.  " + model.Count + " tuples remain." );
+            else
+                Console.WriteLine( "Removal of tuples maching input pattern failed or no tuples were matched." );
+        }
+
+        private void OutputAddOperation( TextWriter outStream, string[] commandTokens ) {
+            string output = string.Empty;
+            string[] tuple = new string[ commandTokens.Length - 1 ];
+            Array.Copy( commandTokens, 1, tuple, 0, tuple.Length );
+
+            if ( model.Enter( tuple ) ) {
+                for ( int i = 0; i < tuple.Length; ++i ) {
+                    output += tuple[ i ];
+                    if ( i + 1 < tuple.Length )
+                        output += ", ";
+                }
+
+                outStream.WriteLine( "Tuple (" + output + ") successfully stored or replaced.  Total tuple count: " + model.Count );
+            }
+            else {
+                outStream.WriteLine( "Error adding tuple, does it already exist?" );
+            }
+        }
+
         private void OutputInstructions( TextWriter outStream ) {
-            outStream.WriteLine( "Available commands:\n\tadd [word]...\n\textract value-position key-position pattern\n\tdelete key-position pattern" );
+            outStream.WriteLine( "Available commands:\n\tadd [word]...\n\textract value-position key-position pattern\n\tdelete key-position pattern\n\tquit\n" );
         }
 
         static void Main( string[] args ) {
-            
-
-            
-
+            DBTestApp tester = new DBTestApp();
+            tester.CommandParseLoop( Console.Out, Console.In );         
         }
     }
 }
