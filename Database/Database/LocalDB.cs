79a5d23ceeb8e6f82ac9a6ef5c7fe35883188d9f
atv7683 <atv7683@099f2f60-5456-0410-b76e-8cc51cc0753e>
2008-10-23 00:24:36 +0000

    -Use string.equals for comparison rather than reference compare -Add some comments

--- Database/Database/LocalDB.cs
+++ Database/Database/LocalDB.cs
@@ -6,17 +6,26 @@ using Axel.Database;
 
 namespace TerryAndMike.Database
 {
+    /// <summary>
+    /// Implements the IModel façade interface, abstracts read/writes to the DB
+    /// </summary>
     public class LocalDB : IModel
     {
+        /// <summary>
+        /// Backend database, stores all tuples and iterates over them to perform queries
+        /// </summary>
         protected IDB database = new DB();
 
         #region IModel Members
 
+        /// <summary> number of entries in database. </summary>
         public int Count
         {
             get { return database.Count; }
         }
 
+        /// <summary> finds matching tuples. </summary>
+        /// <returns> words to be shown in each field. </returns>
         public string[][] Search(string[] keys)
         {
             object[] result = database.Extract( tryMatchTuple => MatchTuples(keys,tryMatchTuple) , reportedTuple => reportedTuple);
@@ -30,23 +39,33 @@ namespace TerryAndMike.Database
             return strRet;
         }
 
+        /// <summary> adds (or replaces) a tuple. </summary>
+        /// <returns> true if something was added (not replaced). </returns>
         public bool Enter(string[] tuple)
         {
             return database.Add( tryMatchTuple => MatchTuples(tuple,tryMatchTuple), tuple );
         }
 
+        /// <summary> removes tuples. </summary>
+        /// <returns> returns true if something was removed. </returns>
         public bool Remove(string[] keys)
         {
             return database.Delete( tryMatchTuple => MatchTuples(keys,tryMatchTuple) ) > 0;
         }
 
-        protected static bool MatchTuples(object[] searchTuple, object[] dbTuple)
+        /// <summary>
+        /// Compares two sets of tuples
+        /// </summary>
+        /// <param name="searchTuple">Tuple from Contoller to perform matching with.</param>
+        /// <param name="dbTuple">Tuple from the database</param>
+        /// <returns>boolean, true if tuples match</returns>
+        protected static bool MatchTuples(string[] searchTuple, object[] dbTuple)
         {
             if (searchTuple.Length != dbTuple.Length) return false;
 
             for (int i = 0; i < searchTuple.Length; ++i) {
-                //if search tuple value is not null and doesn't match, return false
-                if (searchTuple[i] != null && searchTuple[i] != dbTuple[i])
+                //if search tuple value is not null and doesn't match (string.equals), return false
+                if (searchTuple[i] != null && searchTuple[i] != (string)dbTuple[i])
                     return false;
             }
 
