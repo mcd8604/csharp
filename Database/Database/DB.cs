7f2e3c7f200ceb851398ee644d123760c702d388
atv7683 <atv7683@099f2f60-5456-0410-b76e-8cc51cc0753e>
2008-10-23 02:41:15 +0000

    check if tuple is deleted in Search(), throws off iteration otherwise

--- Database/Database/DB.cs
+++ Database/Database/DB.cs
@@ -3,18 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace TerryAndMike.Database
-{
+namespace TerryAndMike.Database {
     /// <summary>
     /// Implementation of a "Poor Man's" database
     /// </summary>
-    public class DB: IDB
-    {
+    public class DB : IDB {
         /// <summary>
         /// Performs the result of a match.
         /// </summary>
         /// <param name="matchedTuple">The tuple that was matched.</param>
-        private delegate void MatchResult(object[] matchedTuple);
+        private delegate void MatchResult( object[] matchedTuple );
 
         /// <summary>
         /// The list of tuples in the database.
@@ -25,17 +23,23 @@ namespace TerryAndMike.Database
         /// Iterates over tuples, calls result for each successful match.
         /// </summary>
         /// <param name="match">Delegate to perform match.</param>
-        /// <param name="result">Delegate to perform result of a match.</param>
+        /// <param name="result">Delegate to perform result of a match. If result is going to delete tuple(s) it must only
+        /// delete tuple passed as parameter or all tuples. </param>
         /// <returns>Returns number of matched tuples.</returns>
-        private int Search(Match match, MatchResult result)
-        {
+        private int Search( Match match, MatchResult result ) {
             int matches = 0;
+            object[] tuple;
 
-            for (int i = 0; i < tuples.Count; ++i)
-            {
-                if (match(tuples[i]))
-                {
-                    if (result != null) result(tuples[i]); 
+            for ( int i = 0; i < tuples.Count; ++i ) {
+                if ( match( tuples[ i ] ) ) {
+                    if ( result != null ) {
+                        tuple = tuples[ i ];
+                        result( tuple );
+
+                        //if tuple was deleted
+                        if ( i < tuples.Count && tuple != tuples[ i ] )
+                            --i;
+                    }
                     ++matches;
                 }
             }
@@ -49,15 +53,14 @@ namespace TerryAndMike.Database
         /// Adds a tuple and returns true if it replaces a tuple with equal content as determined by the Match argument.
         /// </summary>
         /// <param name="match">The delegate to specify how a tuple matches.</param>
-        /// <param name="tuple"></param>
-        /// <returns></returns>
-        bool IDB.Add(Match match, object[] tuple)
-        {
+        /// <param name="tuple">The tuple to inset</param>
+        /// <returns>True if a tuple was replaced, false if it was just inserted</returns>
+        bool IDB.Add( Match match, object[] tuple ) {
             // First validate tuple 
             foreach(object o in tuple) if(o == null) return false;
 
             // Replace each matched tuple
-            int numMatched = Search(match, matchedTuple => { for (int i = 0; i < tuple.Length; ++i) matchedTuple[i] = tuple[i]; });
+            int numMatched = Search( match, matchedTuple => { for ( int i = 0; i < tuple.Length; ++i ) matchedTuple[ i ] = tuple[ i ]; } );
 
             //add new tuple
             if (numMatched == 0) tuples.Add(tuple);
@@ -71,11 +74,10 @@ namespace TerryAndMike.Database
         /// <param name="match">The delegate to specify how a tuple matches.</param>
         /// <param name="report">The delegate to perform the formatting of the tuple.</param>
         /// <returns>An array of formatted tuples.</returns>
-        object[] IDB.Extract(Match match, Report report)
-        {
+        object[] IDB.Extract( Match match, Report report ) {
             List<object> result = new List<object>();
 
-            Search(match, tuple => result.Add(report(tuple)));
+            Search( match, tuple => result.Add( report( tuple ) ) );
 
             return result.ToArray();
         }
@@ -85,17 +87,15 @@ namespace TerryAndMike.Database
         /// </summary>
         /// <param name="match">The delegate that specifies how a tuple matches.</param>
         /// <returns>The number of matched tuples.</returns>
-        int IDB.Delete(Match match)
-        {
-            return Search(match, matchedTuple => tuples.Remove(matchedTuple));
+        int IDB.Delete( Match match ) {
+            return Search( match, matchedTuple => tuples.Remove( matchedTuple ) );
         }
 
         /// <summary>
         /// The number of tuples in the database.
         /// </summary>
-        int IDB.Count
-        {
-            get { return tuples.Count;  }
+        int IDB.Count {
+            get { return tuples.Count; }
         }
 
         #endregion
