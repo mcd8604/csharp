70a8e7d4b1ad5fb1e67c64e2a668ddc12805bdae
mcd8604 <mcd8604@users.noreply.github.com>
2008-10-30 23:50:08 +0000

    added connection checking for remote DB

--- Database2/app2/Window1.xaml.cs
+++ Database2/app2/Window1.xaml.cs
@@ -24,8 +24,14 @@ namespace app2
         /// <summary>Creates a new instance of Window1</summary>
         /// <remarks>Creates a controller which connects to this.</remarks>
         public Window1() {
-          InitializeComponent();
-          var controller = new ToggleController(new LocalDB(), new RemoteDB(), new WorkQueue(),
+            InitializeComponent();
+
+            //Init remoteDB
+            RemoteDB remoteDB = new RemoteDB();
+            remoteDB.ConnectionEndpointError += (sender, connErrorArgs) => { MessageBox.Show(connErrorArgs.ErrorMsg, "Connection Error!"); };
+            remoteDB.Connect();
+
+            var controller = new ToggleController(new LocalDB(), remoteDB, new WorkQueue(),
             new Enable(isEnabled => {
               Toggle.IsEnabled = Search.IsEnabled = Enter.IsEnabled = Remove.IsEnabled = isEnabled; }),
             null,
@@ -33,14 +39,14 @@ namespace app2
             new Access(() => Names.Text, s => { Names.Text = s; }),
             new Access(() => Phones.Text, s => { Phones.Text = s; }),
             new Access(() => Rooms.Text, s => { Rooms.Text = s; }));
-          controller.doSize(null, null);
-          Search.Click += new RoutedEventHandler(controller.doSearch);
-          Enter.Click += new RoutedEventHandler(controller.doEnter);
-          Remove.Click += new RoutedEventHandler(controller.doRemove);
-          Size.MouseDown += new MouseButtonEventHandler(controller.doSize);
-          Toggle.Click += new RoutedEventHandler(controller.doToggle);
-          controller.DatabaseToggled += (database) => { dbLabel.Header = database == Databases.ONE ? "Local" : "Remote"; };
-          dbLabel.Header = "Local";
+            controller.doSize(null, null);
+            Search.Click += new RoutedEventHandler(controller.doSearch);
+            Enter.Click += new RoutedEventHandler(controller.doEnter);
+            Remove.Click += new RoutedEventHandler(controller.doRemove);
+            Size.MouseDown += new MouseButtonEventHandler(controller.doSize);
+            Toggle.Click += new RoutedEventHandler(controller.doToggle);
+            controller.DatabaseToggled += (database) => { dbLabel.Header = database == Databases.ONE ? "Local" : "Remote"; };
+            dbLabel.Header = "Local";
         }
     }
 }
--- Database2/remote/RemoteDB.cs
+++ Database2/remote/RemoteDB.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Axel.Database;
 using DBServiceSoapClient = remote.dbservice.DBServiceSoapClient;
 using ArrayOfString = remote.dbservice.ArrayOfString;
+using System.ServiceModel;
 
 namespace TerryAndMike.Database
 {
@@ -11,47 +12,118 @@ namespace TerryAndMike.Database
     public class RemoteDB : DB<string>, IModel
     {
         /// <summary> The database service client </summary>
-        protected DBServiceSoapClient model = new DBServiceSoapClient();
+        protected DBServiceSoapClient model;
+
+        /// <summary>
+        /// Contains connection error event data
+        /// </summary>
+        public class ConnectionErrorArgs : EventArgs {
+            /// <summary>
+            /// The connection error message.
+            /// </summary>
+            protected readonly string errorMsg;
+            /// <summary>
+            /// The connection error message.
+            /// </summary>
+            public string ErrorMsg { get { return errorMsg; } }
+            /// <summary>
+            /// Initializes a new instance of the ConnectionErrorArgs class.
+            /// </summary>
+            /// <param name="msg">The connection error message.</param>
+            public ConnectionErrorArgs(string msg) { this.errorMsg = msg; }
+        }
+
+        /// <summary>
+        /// Raised when a connection endpoint error is found.
+        /// </summary>
+        public event EventHandler<ConnectionErrorArgs> ConnectionEndpointError;
+
+        /// <summary>
+        /// Creates a new RemoteDB client connection. 
+        /// </summary>
+        /// <remarks> Raises a ConnectionError if endpoint of client is not found.</remarks>
+        public void Connect()
+        {
+            model = new DBServiceSoapClient();
+
+            // Test connection
+            Call((CallRoutine<int>)delegate{ return model.get_Count(); });
+        }
 
         /// <summary> number of tuples, delegate to <c>list</c>. </summary>
-        public int Count { get { return model.get_Count(); } }
+        public int Count { get { return Call((CallRoutine<int>)delegate { return model.get_Count(); }); } }
 
         /// <returns> what was found. </returns>>
         public string[][] Search(string[] keys)
         {
-            ArrayOfString aosKeys = new ArrayOfString();
-            foreach (string s in keys)
-                aosKeys.Add(s);
+            return Call((CallRoutine<string[][]>)delegate
+            {
+                ArrayOfString aosKeys = new ArrayOfString();
+                foreach (string s in keys)
+                    aosKeys.Add(s);
 
-            ArrayOfString[] result = model.Search(aosKeys);
+                ArrayOfString[] result = model.Search(aosKeys);
 
-            string[][] retArr = new string[result.Length][];
-            for(int i = 0; i < retArr.Length; ++i)
-                retArr[i] = result[i].ToArray();
-            
-            return retArr;
+                string[][] retArr = new string[result.Length][];
+                for (int i = 0; i < retArr.Length; ++i)
+                    retArr[i] = result[i].ToArray();
+
+                return retArr;
+            });
         }
 
         /// <returns> true if something was added (and not replaced). </returns>
         public bool Enter(string[] tuple)
         {
-            ArrayOfString aosKeys = new ArrayOfString();
-            foreach (string s in tuple)
-                aosKeys.Add(s);
+            return Call((CallRoutine<bool>)delegate
+            {
+                ArrayOfString aosKeys = new ArrayOfString();
+                foreach (string s in tuple)
+                    aosKeys.Add(s);
 
-            return model.Enter(aosKeys);
+                return model.Enter(aosKeys);
+            });
         }
 
         /// <returns> true if something was removed. </returns>
         public bool Remove(string[] keys)
         {
-            ArrayOfString aosKeys = new ArrayOfString();
-            foreach (string s in keys)
-                aosKeys.Add(s);
+            return Call((CallRoutine<bool>)delegate
+            {
+                ArrayOfString aosKeys = new ArrayOfString();
+                foreach (string s in keys)
+                    aosKeys.Add(s);
 
-            return model.Remove(aosKeys);
+                return model.Remove(aosKeys);
+            });
         }
 
+        /// <summary>
+        /// Defines a call routine
+        /// </summary>
+        /// <typeparam name="T">The return type of the call</typeparam>
+        /// <returns>The specified type</returns>
+        private delegate T CallRoutine<T>();
+
+        /// <summary>
+        /// Executes a call routine
+        /// </summary>
+        /// <typeparam name="T">The return type of the call</typeparam>
+        /// <param name="d">The call routine</param>
+        /// <returns>The return of the call routine</returns>
+        private T Call<T>(CallRoutine<T> d)
+        {
+            T retVal = default(T);
+            try
+            {
+                retVal = d();
+            }
+            catch (EndpointNotFoundException)
+            {
+                if (ConnectionEndpointError != null) ConnectionEndpointError(this, new ConnectionErrorArgs("Web service not found, make sure the service is running."));
+            }
+            return retVal;
+        }
     }
 }
 
