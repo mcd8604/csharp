62c7f56e57e4c3bff2a74c5ef3a491f00b504c64
atv7683 <atv7683@099f2f60-5456-0410-b76e-8cc51cc0753e>
2008-11-05 23:06:41 +0000

    Add WebApp

---
+++ Database3/service/WebApp.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="WebApp.aspx.cs" Inherits="WebApp" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title>Database Webpage</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Button ID="btn_toggleDB" runat="server" Text="Toggle DB:" 
+            onclick="btn_toggleDB_Click" />&nbsp;
+        <asp:Label ID="lbl_currentDB" runat="server" Text="Local"></asp:Label>
+    
+    </div>
+    <p>
+        Name:<br />
+        <asp:TextBox ID="txt_name" runat="server" Width="85%" TextMode="MultiLine"></asp:TextBox>
+    </p>
+    <p>
+        Phone:<br />
+        <asp:TextBox ID="txt_phone" runat="server" Width="85%" TextMode="MultiLine"></asp:TextBox>
+    </p>
+    <p>
+        Room:<br />
+        <asp:TextBox ID="txt_room" runat="server" Width="85%" TextMode="MultiLine"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Button ID="btn_search" runat="server" Text="search" 
+            onclick="btn_search_Click" />&nbsp;
+        <asp:Button ID="btn_enter" runat="server" Text="enter" 
+            onclick="btn_enter_Click" />&nbsp;
+        <asp:Button ID="btn_remove" runat="server" Text="remove" 
+            onclick="btn_remove_Click" />&nbsp;
+        <asp:Label ID="lbl_recordCount" runat="server" Text="0"></asp:Label>
+    </p>
+    </form>
+</body>
+</html>
---
+++ Database3/service/WebApp.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Axel.Database;
+
+public partial class WebApp : System.Web.UI.Page
+{
+
+    protected void Page_Init(object sender, EventArgs eventArgs)
+    {
+        /*new Switcher(
+              new Enable(isEnabled =>
+              {
+                  btn_toggleDB.Enabled = btn_search.Enabled = btn_enter.Enabled = btn_remove.Enabled = isEnabled;
+              }),
+              new IAccess[]{
+                  new Access(() => lbl_currentDB.Text, s => { lbl_currentDB.Text = s; }),
+                  new Access(() => lbl_recordCount.Text, s => { lbl_recordCount.Text = s; }),
+                  new Access(() => txt_name.Text, s => { txt_name.Text = s; }),
+                  new Access(() => txt_phone.Text, s => { txt_phone.Text = s; }),
+                  new Access(() => txt_room.Text, s => { txt_room.Text = s; })},
+                  new SetClick[]{
+              new SetClick((EventHandler e) => { btn_toggleDB.Click += new EventHandler(e); }),
+              new SetClick((EventHandler e) => { btn_search.Click += new EventHandler(e); }),
+              new SetClick((EventHandler e) => { btn_enter.Click += new EventHandler(e); }),
+              new SetClick((EventHandler e) => { btn_remove.Click += new EventHandler(e); }),
+                  },
+              new WorkQueue(),
+              "local", new LocalDB(), "remote", new RemoteDB());*/
+
+        Session["DBs"] = new IModel[] { new LocalDB(), new RemoteDB() };
+        Session["activeDB"] = 0;
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //lbl_currentDB.Text = Session["DBs"];//   [0].GetType().Name;
+    }
+
+
+    protected void btn_toggleDB_Click(object sender, EventArgs e)
+    {
+    }
+    protected void btn_search_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btn_enter_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btn_remove_Click(object sender, EventArgs e)
+    {
+        
+    }
+}
